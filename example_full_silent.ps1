4d16404e035e50ffcec76dc71cf96c0afed3dce4
Justin Trantham <jgticy@gmail.com>
2021-09-12 11:25:26 -0500

    example full silent

--- example_full_silent.ps1
+++ example_full_silent.ps1
@@ -1,1 +1,20 @@
+function Install-Windows11 {
+[CmdletBinding()]
+param(
+  [Parameter(Mandatory=$true)]
+  [String]$url,
+  [String]$extraArgs
+  ) 
+$savePath = "c:\windows\temp\win11.iso"
 
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile($url,$savePath);  
+
+$vol = Mount-DiskImage -ImagePath $savePath  -PassThru |
+	Get-DiskImage | 
+	Get-Volume
+$installer = '{0}:\setup.exe' -f $vol.DriveLetter
+Start-Process $installer -Wait -ArgumentList "/auto upgrade /eula accept $extraArgs"
+}
+
+Install-Windows11 -url "https://URL-TO-ISO" -extraArgs "/quiet"
