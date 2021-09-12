3eabaa1a2afb7527a4c6fca25c4a5dbdb8442c46
Justin Trantham <jgticy@gmail.com>
2021-09-12 10:48:58 -0500

    Update and rename Upgrade_Function.ps1 to Upgrade_quiet.ps1
    changed the switch to silent

--- Upgrade_quiet.ps1
+++ Upgrade_quiet.ps1
@@ -3,5 +3,5 @@ $vol = Mount-DiskImage -ImagePath $FilePath  -PassThru |
 	Get-DiskImage | 
 	Get-Volume
 $installer = '{0}:\setup.exe' -f $vol.DriveLetter
-Start-Process $installer -Wait -ArgumentList "/auto upgrade /eula accept"
+Start-Process $installer -Wait -ArgumentList "/auto upgrade /eula accept /quiet"
 }
